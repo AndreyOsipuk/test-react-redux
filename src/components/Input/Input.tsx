0e89617d1fd03a036e233476cdade231b770a2e2
Andrey Osipuk <andrey.o@list.ru>
2024-03-13 15:23:44 +0300

    add fetch for 3 simbols

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,29 +1,31 @@
-import { FC, useRef } from "react";
+import { FC, useEffect, useRef, useState } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import { fetchJokesByQuery } from "../../store/articleSlice";
 import { useAppDispatch, useAppSelector } from "../../store";
 
 export const Input: FC = () => {
-  const inputRef = useRef<HTMLInputElement>(null)
-  const dispatch = useAppDispatch()
-  const total = useAppSelector((state) => state.total)
+  const inputRef = useRef<HTMLInputElement>(null);
+  const dispatch = useAppDispatch();
+  const total = useAppSelector((state) => state.total);
+  const [value, setValue] = useState("");
 
-  const handleSubmit = (ev: React.FormEvent<HTMLFormElement>) => {
-    ev.preventDefault();
-    if(inputRef.current) {
-      dispatch(fetchJokesByQuery(inputRef.current.value))
+  useEffect(() => {
+    if (value.length >= 3) {
+      dispatch(fetchJokesByQuery(value))
     }
-  }
+  }, [value, dispatch]);
 
   return (
-    <Box
-      component="form"
-      noValidate
-      autoComplete="off"
-      onSubmit={handleSubmit}
-    >
-      <TextField autoFocus inputRef={inputRef} label="Outlined" variant="outlined" />
+    <Box component="div">
+      <TextField
+        autoFocus
+        inputRef={inputRef}
+        label="Outlined"
+        variant="outlined"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
 
       {total > 0 && <p>Found jokes: {total}</p>}
     </Box>
